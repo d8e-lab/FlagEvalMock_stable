64d69fc8702b603b6838dac7c73a54ac751a6244
sbc <3403465822@qq.com>
2023-09-14 15:27:03 +0800

    evaluate all datasets with 8 gpus

---
+++ scripts/all_llama2_base_dict.sh
@@ -0,0 +1,11 @@
+MODEL=llama2
+LLAMA_BASE=/data/LLM/Llama-2-7b-hf/
+current_datetime=$(date +"%m%d_%H_%M_%S")
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+torchrun --nproc-per-node 8 main_v2dist.py --dataset-names="ALL" \
+    --model-name $MODEL \
+    --model-path $LLAMA_BASE \
+    --tokenizer-path $LLAMA_BASE \
+    --batch-size 2 \
+    --verbose \
+    > "./logs/merge_${current_datetime}_${MODEL}.log" 2>&1
