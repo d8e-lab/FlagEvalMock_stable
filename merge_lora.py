41d9ad8594daf0bb4234db2c0d01578fea73f5ee
sbc <3403465822@qq.com>
2023-10-11 15:30:28 +0800

    add merge_lora.py

---
+++ merge_lora.py
@@ -0,0 +1,37 @@
+import torch
+from peft import PeftModel
+from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaTokenizer
+ 
+ 
+def apply_lora(model_name_or_path, output_path, lora_path):
+    print(f"Loading the base model from {model_name_or_path}")
+    base = AutoModelForCausalLM.from_pretrained(
+        model_name_or_path, torch_dtype=torch.float16, low_cpu_mem_usage=True
+    )
+    base_tokenizer = LlamaTokenizer.from_pretrained(model_name_or_path)
+ 
+    print(f"Loading the LoRA adapter from {lora_path}")
+ 
+    lora_model = PeftModel.from_pretrained(
+        base,
+        lora_path,
+        torch_dtype=torch.float16,
+    )
+ 
+    print("Applying the LoRA")
+    model = lora_model.merge_and_unload()
+ 
+    print(f"Saving the target model to {output_path}")
+    model.save_pretrained(output_path)
+    base_tokenizer.save_pretrained(output_path)
+
+
+
+model_name_or_path = "/mnt/store/selected-checkpoint/llama2-plus-checkpoints-freeze/checkpoint-20000"
+#'/mnt/store/llama2-checkpoints-plus-continue/checkpoint-2000'
+output_path = "/mnt/store/selected-checkpoint/llama2-plus-checkpoints-freeze/checkpoint-20000_ynbool/merge"
+#'/mnt/SFT_store/xxw/outputs/all_peft/lora_continue/2023-09-18_04-02-42_merge'
+lora_path = "/mnt/store/selected-checkpoint/llama2-plus-checkpoints-freeze/checkpoint-20000_ynbool/checkpoint-147600/"
+#'/mnt/SFT_store/xxw/outputs/all_peft/lora_continue/2023-09-18_04-02-42'
+
+apply_lora(model_name_or_path, output_path, lora_path)
