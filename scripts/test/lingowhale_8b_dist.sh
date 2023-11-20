d3b15e4f17d018132db67f889e2a019c3ed693a6
sbc <3403465822@qq.com>
2023-11-20 20:25:48 +0800

    add log

--- scripts/test/lingowhale_8b_dist.sh
+++ scripts/test/lingowhale_8b_dist.sh
@@ -1,11 +1,11 @@
 MODEL=llama2
 # tokenizer=/mnt/store/internlm-20b/
-LLAMA_BASE=/mnt/42_store/LLMs/LingoWhale-8B/
+LLAMA_BASE=/mnt/40_store/LLM/LingoWhale-8B/
 mkdir -p "./evaluation_results/$(basename ${LLAMA_BASE})"
 current_datetime=$(date +"%m%d_%H_%M_%S")
 mkdir -p "./logs/${MODEL}/$(basename ${LLAMA_BASE})"
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
-torchrun --master-port 29556 --nproc-per-node 6 main_v2dist.py --dataset-names="ALL" \
+torchrun --master-port 29556 --nproc-per-node 6 main_v2dist.py --dataset-names="RAFT,CSL,CLUEWSC,BUSTM" \
     --model-name $MODEL \
     --model-path $LLAMA_BASE \
     --tokenizer-path $LLAMA_BASE \
